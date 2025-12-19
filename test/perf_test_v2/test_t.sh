c50939c416c81f46fcdb7d149de00f47755345b4
julia-224 <animationkoreandrama@gmail.com>
2025-12-19 04:42:17 +0100

    Performance tests v2 for comparison

--- test/perf_test_v2/test_t.sh
+++ test/perf_test_v2/test_t.sh
@@ -3,8 +3,9 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 prog="$SCRIPT_DIR/../../build/main"
-indir="$SCRIPT_DIR/infiles"
-regex="$SCRIPT_DIR/regex/regexes.rgx"
+
+indir="$1"
+regex="$2"
 
 hs_time=$(
   ${prog} -f "${indir}" -r "${regex}" -e hs -t 2>/dev/null \
