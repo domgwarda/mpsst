679c04c7549a33852eb08abbc354a14826753d74
Kamila Korczynska <346611@uwr.edu.pl>
2026-01-06 15:27:21 +0100

    Removed unused/invalid tests (main_perf_test) and fixed a bug in perf_test_v2/test_.sh

--- test/perf_test_v2/test_.sh
+++ test/perf_test_v2/test_.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 prog="$SCRIPT_DIR/../../build/main"
 
-DATAFILE="$1"
-REGEXFILE="$2"
+indir="$1"
+regex="$2"
 
 start_hs=$(date +%s%3N)   
 ${prog} -f "${indir}" -r "${regex}" -e hs >/dev/null 2>&1
