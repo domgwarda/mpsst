48a236614c33ee008d38fcb2ca10117657cd6d22
domgwarda <domi.gwarda@gmail.com>
2026-02-03 16:23:28 +0100

    Change plot

--- test/perf_test_v2/test6/plot.py
+++ test/perf_test_v2/test6/plot.py
@@ -4,15 +4,17 @@ import matplotlib.pyplot as plt
 
 df_hs = pd.read_csv('res_hs.txt')
 df_pcre = pd.read_csv('res_pcre.txt')
+df_hs['time'] = df_hs['time']/1000
+df_pcre['time'] = df_pcre['time']/1000
 
 print(df_hs.columns)
 
-sns.lineplot(data=df_hs, x='files', y='time', marker='o', c='green', label='HS')
+sns.lineplot(data=df_hs, x='files', y='time', marker='o', c='green', label='HyperScan')
 sns.lineplot(data=df_pcre, x='files', y='time', marker='o', c='red', label='PCRE')
 plt.xlabel('n Files')
-plt.ylabel('Time (in milisecounds)')
+plt.ylabel('Time (in seconds)')
 plt.title('Lineplot -- many files (max 3mb) vs many regexes (~300)')
 plt.legend()
 plt.grid()
 # Zapis wykresu do pliku PNG
-plt.savefig('wykres_hs.png')
+plt.savefig('hs_300rgx_4000files.png')
